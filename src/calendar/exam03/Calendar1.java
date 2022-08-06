1e1ed54f0e97db49c5679a261bfe3429a974c61c
ooo33 <ooo33@192.168.219.104>
2022-08-06 18:06:55 +0900

    Update exam03/Calendar1_Swicth

--- src/calendar/exam03/Calendar1.java
+++ src/calendar/exam03/Calendar1.java
@@ -11,11 +11,6 @@ public class Calendar1 {
 		month = scanner.nextInt();
 
 		switch(month) {
-			case 1:case 3:case 5:case 7:case 8:case 12:{
-				System.out.println(month + "월은 " + "31일까지 있습니다.");
-				break;
-			}
-			
 			case 2:{
 				System.out.println(month + "월은 " + "28일까지 있습니다.");
 				break;
@@ -23,6 +18,11 @@ public class Calendar1 {
 			
 			case 4:case 6:case 9:case 11:{
 				System.out.println(month + "월은 " + "30일까지 있습니다.");
+				break;
+			}
+			
+			default:{
+				System.out.println(month + "월은 " + "31일까지 있습니다.");
 				break;
 			}
 				
